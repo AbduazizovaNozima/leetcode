39237d8514a23f3636d09c663f9d72c5960c5711
Nozima <abduazizovanozima7@gmail.com>
2025-06-03 15:18:36 +0500

    news

---
+++ easy/144.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root):
+        result = []
+        def inorder(node):
+            if node is None:
+                return
+            result.append(node.val)
+            inorder(node.left)
+            inorder(node.right)
+        inorder(root)
+        return result
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.right.left = TreeNode(6)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(8)
+root.right.right.left = TreeNode(9)
+
+s = Solution()
+print(s.inorderTraversal(root))
