d1ce65b21f09d4aec05eb060c5235dafdad0d9bc
Nozima <abduazizovanozima7@gmail.com>
2025-06-04 14:46:51 +0500

    news

--- easy/1.py
+++ easy/1.py
@@ -8,3 +8,4 @@ class Solution:
                     result.append(j)
                     return result
         return []
+
---
+++ medium/15.py
@@ -0,0 +1,13 @@
+# nums = [-1, 0, 1, 2, -1, -4]
+# result = []
+#
+# for i in range(len(nums)):
+#     for j in range(i+1, len(nums)):
+#         for k in range(j+1, len(nums)):
+#             triplet = sorted([nums[i], nums[j], nums[k]])
+#             if sum(triplet) == 0 and triplet not in result:
+#                 result.append(triplet)
+#
+# print(result)
+
+
